1d298193b36b8d61b7ff8e92c9f148d51940ef2b
Mary Flagstad <mflagstad@cj.com>
2016-08-27 18:14:25 -0700

    Setting up outline for titanic workshop

---
+++ titanic/test/titanic_test_suite.py
@@ -0,0 +1,24 @@
+import unittest
+
+
+class TitanicWorkshop(unittest.TestCase):
+
+    #Try using features related to Cabin
+
+    #See if any family size features might help --
+    #do the number of women in a family make the whole family more likely to survive?
+
+    #Does the national origin of the passenger's name have anything to do with survival?
+
+    #Try the random forest classifier in the ensemble.
+
+    #A support vector machine might work well with this data.
+
+    #We could try neural networks.
+
+    #Boosting with a different base classifier might work better.
+
+    #Could majority voting be a better ensembling method than averaging probabilities?
+
+if __name__ == '__main__':
+    unittest.main()
