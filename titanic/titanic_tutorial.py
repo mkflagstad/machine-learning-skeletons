33e3d63a299672b6895124dd494d5003ea010b66
Joe Vargas <jxv@users.noreply.github.com>
2016-09-07 21:11:53 -0700

    Update titanic_tutorial.py

--- titanic/titanic_tutorial.py
+++ titanic/titanic_tutorial.py
@@ -9,8 +9,10 @@ def add_women_in_family(df):
     #Create a copy of the data frame to avoid side effects:
     copy = df.copy(deep=True)
     
-    #Use the columns "Sex" and "FamilyId". Remember that women are represented by a 1
-    #in the sex column. For the -1 family id, make the num women 0.
+    #Use the columns "Sex" and "FamilyId".
+    #Remember that women are represented by a 1 in the sex column.
+    #The person is not in a family if their FamilyId is -1.
+    #In this case assign the number of woman to be 0.
 
     #You can iterate over the index and rows in a dataframe using:
     # for index, row in df.iterrows
