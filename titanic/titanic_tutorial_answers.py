6f041f82ac5d4d646b27a337c2c133a0b3404c7a
Mary Flagstad <mflagstad@cj.com>
2016-09-18 13:37:03 -0700

    Comparing expected and solution for random forest ensemble WIP

--- titanic/titanic_tutorial_answers.py
+++ titanic/titanic_tutorial_answers.py
@@ -1,5 +1,6 @@
 from __future__ import print_function
 from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.linear_model import LogisticRegression
 from sklearn.cross_validation import KFold
 import pandas
@@ -30,14 +31,17 @@ def add_women_in_family(df):
 def random_forest_ensemble(df):
     algorithms = [
     [GradientBoostingClassifier(random_state=1, n_estimators=25, max_depth=3), ["Pclass", "Sex", "Age", "Fare", "Embarked", "FamilySize", "Title", "FamilyId"]],
-    [LogisticRegression(random_state=1), ["Pclass", "Sex", "Fare", "FamilySize", "Title", "Age", "Embarked"]]]
+    [LogisticRegression(random_state=1), ["Pclass", "Sex", "Fare", "FamilySize", "Title", "Age", "Embarked"]],
+    [RandomForestClassifier(random_state=1, n_estimators=50, min_samples_split=4, min_samples_leaf=2),["Pclass", "Sex", "Age", "Fare", "Embarked", "FamilySize", "Title", "FamilyId"]]]
+    result = ensemble(df, algorithms)
+    print("result size: ", result.shape)
+    return result
 
-    # Initialize the cross validation folds
+def ensemble(df, algorithms_with_predictors):
     kf = KFold(df.shape[0], n_folds=3, random_state=1)
 
     # algorithms x data_points (2 x 891)
-    predictions = np.asarray([np.asarray([]) for i in range(len(algorithms))])
-    print('predictions shape: ', predictions.shape)
+    predictions = np.asarray([np.asarray([]) for i in range(len(algorithms_with_predictors))])
     for train, test in kf:
         train_target = df["Survived"].iloc[train]
 
@@ -45,21 +49,13 @@ def random_forest_ensemble(df):
         full_test_predictions = []
 
         # Make predictions for each algorithm on each fold
-        for alg, predictors in algorithms:
+        for alg, predictors in algorithms_with_predictors:
             # Fit the algorithm on the training data.
             alg.fit(df[predictors].iloc[train,:], train_target)
             # Select and predict on the test fold.
             # The .astype(float) is necessary to convert the dataframe to all floats and avoid an sklearn error.
-          
             test_predictions = alg.predict_proba(df[predictors].iloc[test,:].astype(float))[:,1]
-            print("test_predictions shape: ", test_predictions.shape)
             full_test_predictions.append(test_predictions)
-
-        print(len(full_test_predictions))
-
         full_test_predictions = np.asarray(full_test_predictions)
-        print("full_test_predictions size: ", full_test_predictions.shape)
         predictions = np.concatenate((predictions, full_test_predictions), axis=1)
-        print("predictions size: ", predictions.shape)
-
-    print("predictions: ", predictions)
+    return predictions
--- titanic/titanic_tutorial_test_suite.py
+++ titanic/titanic_tutorial_test_suite.py
@@ -2,6 +2,7 @@ import unittest
 import titanic_tutorial
 import titanic_tutorial_answers
 import pandas
+import numpy as np
 from pandas.util.testing import assert_frame_equal
 from titanic_preprocess import preprocess
 
@@ -16,18 +17,28 @@ def is_df_equal(df1, df2):
     except:  # appeantly AssertionError doesn't catch all
         return False
 
+
+
 class TitanicWorkshop(unittest.TestCase):
 
+    def assert_array_equal(self, actual, expected):
+        equal = np.array_equal(expected, actual)
+        if not equal:
+            print("Expected: ")
+            print(expected)
+            print("Actual: ")
+            print(actual)
+        self.assertTrue(equal)
 
     def test_add_women_in_family(self):
         your_soln = titanic_tutorial.add_women_in_family(titanic_train)
-        print(titanic_train.iloc[0])
         expected = titanic_tutorial_answers.add_women_in_family(titanic_train)
-        print(titanic_train.iloc[0])
         self.assertTrue(is_df_equal(your_soln, expected))
 
     def test_random_forest_ensemble(self):
-        titanic_tutorial_answers.random_forest_ensemble(titanic_train)
+        your_soln = titanic_tutorial.random_forest_ensemble(titanic_train)
+        expected = titanic_tutorial_answers.random_forest_ensemble(titanic_train)
+        self.assert_array_equal(your_soln, expected)
 
     #Try the random forest classifier in the ensemble.
 
